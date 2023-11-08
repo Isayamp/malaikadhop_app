8160a3a7dffd3a87e35b0b76dcb3fd81ce2f8aa2
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-11-08 11:07:16 +0200

    add asset helper to assets

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,23 +5,23 @@
     <!--favicon-->
     <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
     <!--plugins-->
-    <link href="{! asset('assets/plugins/simplebar/css/simplebar.css') !}" rel="stylesheet" />
-    <link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-    <link href="assets/plugins/highcharts/css/highcharts.css" rel="stylesheet" />
-    <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
-    <link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+    <link href="{!! asset('assets/plugins/simplebar/css/simplebar.css') !!}" rel="stylesheet" />
+    <link href="{!! asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') !!}" rel="stylesheet" />
+    <link href="{!! asset('assets/plugins/highcharts/css/highcharts.css') !!}" rel="stylesheet" />
+    <link href="{!! asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') !!}" rel="stylesheet" />
+    <link href="assets/plugins/metismenu/css/metisMenu.min.css') !!}" rel="stylesheet" />
     <!-- loader-->
-    <link href="assets/css/pace.min.css" rel="stylesheet" />
-    <script src="assets/js/pace.min.js"></script>
+    <link href="{!! asset('assets/css/pace.min.css') !!}" rel="stylesheet" />
+    <script src="{!! asset('assets/js/pace.min.js') !!}"></script>
     <!-- Bootstrap CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/bootstrap-extended.css" rel="stylesheet">
+    <link href="{!! asset('assets/css/bootstrap.min.css') !!}" rel="stylesheet">
+    <link href="{!! asset('assets/css/bootstrap-extended.css') !!}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">
-    <link href="assets/css/app.css" rel="stylesheet">
-    <link href="assets/css/icons.css" rel="stylesheet">
+    <link href="{!! asset('assets/css/app.css') !!}" rel="stylesheet">
+    <link href="{!! asset('assets/css/icons.css') !!}" rel="stylesheet">
     <!-- Theme Style CSS -->
-    <link rel="stylesheet" href="assets/css/dark-theme.css" />
-    <link rel="stylesheet" href="assets/css/semi-dark.css" />
-    <link rel="stylesheet" href="assets/css/header-colors.css" />
-    <title>Synadmin – Bootstrap5 Admin Template</title>
+    <link rel="stylesheet" href="{!! asset('assets/css/dark-theme.css') !!}" />
+    <link rel="stylesheet" href="{!! asset('assets/css/semi-dark.css') !!}" />
+    <link rel="stylesheet" href="{!! asset('assets/css/header-colors.css') !!}" />
+    <title>{{ config('app.name') }}</title>
 </head>
