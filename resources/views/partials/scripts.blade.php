2993ec57b6f5e26b1b95d052bf880a96770533b2
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-11-13 18:26:01 +0200

    implement scripts partials

---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,23 @@
+<!-- Bootstrap JS -->
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<!--plugins-->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
+<script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
+<script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+<script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
+<script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="assets/plugins/highcharts/js/highcharts.js"></script>
+<script src="assets/plugins/highcharts/js/exporting.js"></script>
+<script src="assets/plugins/highcharts/js/variable-pie.js"></script>
+<script src="assets/plugins/highcharts/js/export-data.js"></script>
+<script src="assets/plugins/highcharts/js/accessibility.js"></script>
+<script src="assets/plugins/apexcharts-bundle/js/apexcharts.min.js"></script>
+<script src="assets/js/index2.js"></script>
+<!--app JS-->
+<script src="assets/js/app.js"></script>
+<script>
+    new PerfectScrollbar('.customers-list');
+    new PerfectScrollbar('.store-metrics');
+    new PerfectScrollbar('.product-list');
+</script>
