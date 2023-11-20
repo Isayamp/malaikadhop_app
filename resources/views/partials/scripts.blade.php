a825b11fae256ed44e8ed63bb74106ca1210fb70
Isaya M Pombo <isayamlelapombo@gmail.com>
2023-11-20 22:24:59 +0200

    sett asset helper

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,21 +1,21 @@
 <!-- Bootstrap JS -->
-<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="{!! asset('assets/js/bootstrap.bundle.min.js') !!}"></script>
 <!--plugins-->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
-<script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
-<script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
-<script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-<script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-<script src="assets/plugins/highcharts/js/highcharts.js"></script>
-<script src="assets/plugins/highcharts/js/exporting.js"></script>
-<script src="assets/plugins/highcharts/js/variable-pie.js"></script>
-<script src="assets/plugins/highcharts/js/export-data.js"></script>
-<script src="assets/plugins/highcharts/js/accessibility.js"></script>
-<script src="assets/plugins/apexcharts-bundle/js/apexcharts.min.js"></script>
-<script src="assets/js/index2.js"></script>
+<script src="{!! asset('assets/js/jquery.min.js') !!}"></script>
+<script src="{!! asset('assets/plugins/simplebar/js/simplebar.min.js') !!}"></script>
+<script src="{!! asset('assets/plugins/metismenu/js/metisMenu.min.js') !!}"></script>
+<script src="{!! asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') !!}"></script>
+<script src="{!! asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') !!}"></script>
+<script src="{!! asset('assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') !!}"></script>
+<script src="{!! asset('assets/plugins/highcharts/js/highcharts.js') !!}"></script>
+<script src="{!! asset('assets/plugins/highcharts/js/exporting.js') !!}"></script>
+<script src="{!! asset('assets/plugins/highcharts/js/variable-pie.js') !!}"></script>
+<script src="{!! asset('assets/plugins/highcharts/js/export-data.js') !!}"></script>
+<script src="{!! asset('assets/plugins/highcharts/js/accessibility.js') !!}"></script>
+<script src="{!! asset('assets/plugins/apexcharts-bundle/js/apexcharts.min.js') !!}"></script>
+<script src="{!! asset('assets/js/index2.js') !!}"></script>
 <!--app JS-->
-<script src="assets/js/app.js"></script>
+<script src="{!! asset('assets/js/app.js') !!}"></script>
 <script>
     new PerfectScrollbar('.customers-list');
     new PerfectScrollbar('.store-metrics');
